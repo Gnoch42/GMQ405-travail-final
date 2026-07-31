c6b90ae714749fe28748b22366e088243fbdd2d7
fortoine <fortoine@outlook.com>
2026-07-31 10:27:23 -0400

    Ajout de la structuration du tableau général par années en caractères et en entiers

--- main.R
+++ main.R
@@ -1,6 +1,8 @@
 library(yaml)
+library(sf)
+library(dplyr)
 
-cfg <- read_yaml("ignore/config.yaml")
+cfg <- read_yaml("config.yaml")
 
 
 # Extraction de la donnée pour les régions d'intérêt. Traitement lourd fait une seule fois
@@ -13,3 +15,17 @@ if(!file.exists(cfg$extraction$TBE$outpath) | cfg$extraction$TBE$force_extractio
 #   source("src/data_extraction.R")
 #   extract.ieqm.data(cfg)
 # }
+
+tbe <- st_read(cfg$target$source)
+study.zone <- st_read(cfg$study_zone$source, layer = cfg$study_zone$layer)
+study.zone <- subset(study.zone, st_drop_geometry(study.zone)[,cfg$study_zone$field] %in% cfg$study_zone$regions)
+
+# Construiction de la grille hexagonale ========================================
+source("src/structuration.R")
+
+# Avec les niveau en texte
+tbe.grid.chr <- aggregate_tbe_levels_to_hex(study.zone, tbe, cfg)
+
+# Avec les niveaux en entiers
+tbe.grid.int <- convert_level(tbe.grid)
+
---
+++ src/structuration.R
@@ -0,0 +1,36 @@
+library(dplyr)
+
+aggregate_tbe_levels_to_hex <- function(study.zone, data, cfg) {
+  source("src/hex_grid.R")
+  grid <- create.hex.grid(study.zone, cfg$hex_grid$cellsize)
+  
+  for (year in seq(cfg$lisa_analysis$start, cfg$lisa_analysis$end, 1)) {
+    field.name <- paste0(cfg$target$value_field, "_",year)
+    grid <- aggregate.to.hex(
+      hex = grid,
+      source = subset(data, ANNEE == year),
+      value_field = cfg$target$value_field,
+      value_type = "categorical",
+      out_name = field.name
+    )
+  }
+  return(grid)
+}
+
+convert_levels_col <- function(x) {
+  dplyr::case_when(
+    is.na(x) ~ "0",
+    x %in% c("Leger", "Léger") ~ "1",
+    x %in% c("Modere", "Modéré") ~ "2",
+    x == "Grave" ~ "3",
+    .default = x
+  )
+}
+
+convert_level <- function(x) {
+  x <- x %>%
+    dplyr::mutate(
+      dplyr::across(starts_with("Niveau_"), convert_levels_col)
+    )
+  return(x)
+}
